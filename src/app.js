6c01ea94178528fc7c1cb57edee5766482bd4f7a
James Benson <jjbenson85@gmail.com>
2019-04-12 02:17:54 +0100

    Moved Tone.loop, added pitch controls, refactored settings

--- src/app.js
+++ src/app.js
@@ -14,210 +14,17 @@ class App extends React.Component {
     super()
 
     this.state={
-      transport: {
-        beat: 0,
-        time: 0
-      },
-      instrument: [
-        {
-          id: 0,
-          sequence: [
-            {
-              beat: 0,
-              pitch: 'C3',
-              duration: '32n'
-            },
-            {
-              beat: 1,
-              pitch: 'D3',
-              duration: '32n'
-            },
-            {
-              beat: 2,
-              pitch: 'E3',
-              duration: '32n'
-            },
-            {
-              beat: 3,
-              pitch: 'C3',
-              duration: '32n'
-            },
-            {
-              beat: 4,
-              pitch: 'F3',
-              duration: '32n'
-            },
-            {
-              beat: 5,
-              pitch: 'G3',
-              duration: '32n'
-            },
-            {
-              beat: 6,
-              pitch: 'A3',
-              duration: '32n'
-            },
-            {
-              beat: 7,
-              pitch: 'B3',
-              duration: '32n'
-            },
-            {
-              beat: 8,
-              pitch: 'C3',
-              duration: '32n'
-            },
-            {
-              beat: 9,
-              pitch: 'B3',
-              duration: '32n'
-            },
-            {
-              beat: 10,
-              pitch: 'A3',
-              duration: '32n'
-            },
-            {
-              beat: 11,
-              pitch: 'G3',
-              duration: '32n'
-            },
-            {
-              beat: 12,
-              pitch: 'F3',
-              duration: '32n'
-            },
-            {
-              beat: 13,
-              pitch: 'E3',
-              duration: '32n'
-            },
-            {
-              beat: 14,
-              pitch: 'D3',
-              duration: '32n'
-            },
-            {
-              beat: 15,
-              pitch: 'C3',
-              duration: '32n'
-            }
-          ],
-          settings:{
-
-          }
-
-        },{
-          id: 1,
-          sequence: [
-            {
-              beat: 0,
-              pitch: 'F5',
-              duration: '32n'
-            },
-            {
-              beat: 1,
-              pitch: 'A4',
-              duration: '32n'
-            },
-            {
-              beat: 2,
-              pitch: 'B5',
-              duration: '32n'
-            },
-            {
-              beat: 3,
-              pitch: 'C4',
-              duration: '32n'
-            },
-            {
-              beat: 4,
-              pitch: 'F5',
-              duration: '32n'
-            },
-            {
-              beat: 5,
-              pitch: 'G4',
-              duration: '32n'
-            },
-            {
-              beat: 6,
-              pitch: 'A5',
-              duration: '32n'
-            },
-            {
-              beat: 7,
-              pitch: 'B4',
-              duration: '32n'
-            },
-            {
-              beat: 8,
-              pitch: 'F5',
-              duration: '32n'
-            },
-            {
-              beat: 9,
-              pitch: 'B4',
-              duration: '32n'
-            },
-            {
-              beat: 10,
-              pitch: 'A5',
-              duration: '32n'
-            },
-            {
-              beat: 11,
-              pitch: 'G4',
-              duration: '32n'
-            },
-            {
-              beat: 12,
-              pitch: 'F5',
-              duration: '32n'
-            },
-            {
-              beat: 13,
-              pitch: 'E4',
-              duration: '32n'
-            },
-            {
-              beat: 14,
-              pitch: 'D5',
-              duration: '32n'
-            },
-            {
-              beat: 15,
-              pitch: 'C4',
-              duration: '32n'
-            },
-            {
-              beat: 16,
-              pitch: 'B5',
-              duration: '32n'
-            }
-          ]
-        }
-      ]
+      instruments: ['Monosynth', 'Monosynth']
     }
   }
 
 
-  componentDidMount(){
-    const that = this
-    this.loop = new Tone.Sequence(function(time, beat){
-      that.setState({transport: {beat, time}})
-    }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], '16n')
-
-  }
-
   playSound(){
     Tone.Transport.start()
-    this.loop.start()
 
   }
   stopSound(){
     Tone.Transport.stop()
-    this.loop.stop()
   }
 
   render() {
@@ -226,20 +33,7 @@ class App extends React.Component {
         <button onClick={()=>this.playSound()}>PLAY</button>
         <button onClick={()=>this.stopSound()}>STOP</button>
         <div>
-          <Monosynth
-            id="1"
-            time={this.state.transport.time}
-            pitch={this.state.instrument[0].sequence[this.state.transport.beat].pitch}
-            duration={this.state.instrument[0].sequence[this.state.transport.beat].duration}
-          />
-
-
-          <Monosynth
-            id="2"
-            time={this.state.transport.time}
-            pitch={this.state.instrument[1].sequence[this.state.transport.beat].pitch}
-            duration={this.state.instrument[1].sequence[this.state.transport.beat].duration}
-          />
+          {this.state.instruments.map( (inst,i) => <Monosynth key={i} id={i} />)}
         </div>
 
       </div>
--- src/components/Monosynth.js
+++ src/components/Monosynth.js
@@ -9,29 +9,113 @@ class Monosynth extends React.Component {
     super()
 
     this.state={
-      'oscillator': {
-        'type': 'pwm',
-        'modulationFrequency': 0.2
-      },
-      'envelope': {
-        'attack': 0.52,
-        'decay': 0.1,
-        'sustain': 0.2,
-        'release': 0.9
-      },
-      'filterEnvelope': {
-        'attack': 0.06 ,
-        'decay': 0.2 ,
-        'sustain': 0.5 ,
-        'release': 2 ,
-        'baseFrequency': 200 ,
-        'octaves': 7 ,
-        'exponent': 2
-      },
-      'filter': {
-        'Q': 6,
-        'type': 'lowpass',
-        'rolloff': -24
+      sequence: [
+        {
+          beat: 0,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 1,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 2,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 3,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 4,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 5,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 6,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 7,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 8,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 9,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 10,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 11,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 12,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 13,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 14,
+          pitch: 64,
+          duration: '16n'
+        },
+        {
+          beat: 15,
+          pitch: 64,
+          duration: '16n'
+        }
+      ],
+      settings: {
+        oscillator: {
+          type: 'pwm',
+          modulationFrequency: 0.2
+        },
+        envelope: {
+          attack: 0.01,
+          decay: 0.1,
+          sustain: 0.2,
+          release: 0.1
+        },
+        filterEnvelope: {
+          attack: 0.01 ,
+          decay: 0.2 ,
+          sustain: 0.5 ,
+          release: 0.1 ,
+          baseFrequency: 150 ,
+          octaves: 5 ,
+          exponent: 2
+        },
+        filter: {
+          Q: 1,
+          type: 'lowpass',
+          rolloff: -24
+        }
       }
     }
 
@@ -41,70 +125,109 @@ class Monosynth extends React.Component {
   }
 
   componentDidUpdate(){
-    const envelope = () => {
-      const {attack, decay, sustain, release } = this.state.envelope
-      const env = this.synth.envelope
-      env.attack = attack
-      env.decay = decay
-      env.sustain = sustain
-      env.release = release
-    }
+    // const envelope = () => {
+    //   const {attack, decay, sustain, release } = this.state.envelope
+    //   const env = this.synth.envelope
+    //   env.attack = attack
+    //   env.decay = decay
+    //   env.sustain = sustain
+    //   env.release = release
+    // }
+    //
+    // const filterEnvelope= () => {
+    //   const {attack, decay, sustain, release } = this.state.filterEnvelope
+    //   const env = this.synth.envelope
+    //   env.attack = attack
+    //   env.decay = decay
+    //   env.sustain = sustain
+    //   env.release = release
+    // }
+    //
+    // envelope()
+    // filterEnvelope()
+    //
+    // this.synth.filterEnvelope.baseFrequency = this.state.filterEnvelope.baseFrequency
 
-    const filterEnvelope= () => {
-      const {attack, decay, sustain, release } = this.state.filterEnvelope
-      const env = this.synth.envelope
-      env.attack = attack
-      env.decay = decay
-      env.sustain = sustain
-      env.release = release
-    }
-
-    envelope()
-    filterEnvelope()
-
-    this.synth.filterEnvelope.baseFrequency = this.state.filterEnvelope.baseFrequency
-
-    this.synth.triggerAttackRelease(this.props.pitch, this.props.duration, this.props.time)
+    // this.synth.triggerAttackRelease(this.state.currentSequence.pitch, this.state.currentSequence.duration, this.state.transport.time)
   }
 
   handleControlChange(val, mod){
-    console.log('v', val, this.synth)
-    mod = mod.split('.')
-    // this.synth[mod[0]][mod[1]] = val
+    // mod = mod.split('.')
+    //
+    // const partA = mod[0]
+    // const partB = mod[1]
 
-    let oscillator = {...this.state.oscillator}
-    let envelope = {...this.state.envelope}
-    let filterEnvelope = {...this.state.filterEnvelope}
-    let filter = {...this.state.filter}
-
-    // if(mod[0]==='oscillator'){
-    //   oscillator = {...this.state.oscillator, [mod[1]]: val}
-    // }else if(mod[0]==='envelope'){
-    //   envelope = {...this.state.envelope, [mod[1]]: val}
+    const [partA, partB] = mod.split('.')
+    console.log(partA, partB, val, this.synth)
+    this.synth[partA][partB] = val
+    const settings = {...this.state.settings}
+    settings[partA][partB] = val
+    this.setState({settings})
+    // switch(mod[0]){
+    //   case 'envelope':
+    //     console.log(mod)
+    //     break
+    //
     // }
-    switch(mod[0]){
-      case 'osicllator':
-        oscillator = {...this.state.oscillator, [mod[1]]: val}
-        break
+    //
+    // // this.synth[mod[0]][mod[1]] = val
+    //
+    // let oscillator = {...this.state.oscillator}
+    // let envelope = {...this.state.envelope}
+    // let filterEnvelope = {...this.state.filterEnvelope}
+    // let filter = {...this.state.filter}
+    //
+    // // if(mod[0]==='oscillator'){
+    // //   oscillator = {...this.state.oscillator, [mod[1]]: val}
+    // // }else if(mod[0]==='envelope'){
+    // //   envelope = {...this.state.envelope, [mod[1]]: val}
+    // // }
+    // switch(mod[0]){
+    //   case 'oscillator':
+    //     oscillator = {...this.state.oscillator, [mod[1]]: val}
+    //     break
+    //
+    //   case 'envelope':
+    //     envelope = {...this.state.envelope, [mod[1]]: val}
+    //     break
+    //
+    //   case 'filterEnvelope':
+    //     filterEnvelope = {...this.state.filterEnvelope, [mod[1]]: val}
+    //     break
+    //
+    //   case 'filter':
+    //     filter = {...this.state.filter, [mod[1]]: val}
+    //     break
+    // }
+    //
+    // this.setState({oscillator, envelope, filterEnvelope, filter})
+    // const envelopeF = () => {
+    //   const {attack, decay, sustain, release } = this.state.envelope
+    //   const env = this.synth.envelope
+    //   env.attack = attack
+    //   env.decay = decay
+    //   env.sustain = sustain
+    //   env.release = release
+    // }
+    //
+    // const filterEnvelopeF= () => {
+    //   const {attack, decay, sustain, release } = this.state.filterEnvelope
+    //   const env = this.synth.envelope
+    //   env.attack = attack
+    //   env.decay = decay
+    //   env.sustain = sustain
+    //   env.release = release
+    // }
+    //
+    // envelopeF()
+    // filterEnvelopeF()
+    //
+    // this.synth.filterEnvelope.baseFrequency = this.state.filterEnvelope.baseFrequency
 
-      case 'envelope':
-        envelope = {...this.state.envelope, [mod[1]]: val}
-        break
-
-      case 'filterEnvelope':
-        filterEnvelope = {...this.state.filterEnvelope, [mod[1]]: val}
-        break
-
-      case 'filter':
-        filter = {...this.state.filter, [mod[1]]: val}
-        break
-    }
-
-    this.setState({oscillator, envelope, filterEnvelope, filter})
   }
 
   handleRadioButtonChange(val){
-    console.log('v', val, this.synth)
+    // console.log('v', val, this.synth)
     let str
     switch(val){
       case 0:
@@ -120,16 +243,37 @@ class Monosynth extends React.Component {
         str = 'sine'
         break
     }
-    console.log('str', str)
+    // console.log('str', str)
 
     this.synth.oscillator.type = str
   }
+
+  handlePitchChange(val, beat){
+    // console.log(val, beat)
+    const sequence = [...this.state.sequence]
+    sequence[beat].pitch = val
+    this.setState({sequence})
+  }
   componentDidMount(){
-    this.synth = new Tone.MonoSynth({
-      oscillator: this.state.oscillator,
-      envelope: this.state.envelope,
-      filterEnvelope: this.state.filterEnvelope
-    }).toMaster()
+    const that = this
+    this.loop = new Tone.Sequence(function(time, beat){
+      // that.setState({transport: {beat, time}})
+      // console.log(that.state.sequence[beat])
+      // this.setState({currentSequence: that.props.sequence[beat]})
+      const {pitch, duration} = that.state.sequence[beat]
+      // this.synth.triggerAttackRelease(this.state.currentSequence.pitch, this.state.currentSequence.duration, this.state.transport.time)
+      that.synth.triggerAttackRelease(pitch, duration, time)
+    }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], '16n')
+    this.loop.start()
+
+    // this.synth = new Tone.MonoSynth({
+    //   oscillator: this.state.oscillator,
+    //   envelope: this.state.envelope,
+    //   filterEnvelope: this.state.filterEnvelope
+    // }).toMaster()
+    const settings = this.state.settings
+    this.synth = new Tone.MonoSynth(settings
+    ).toMaster()
 
     const obj = {...this.synth}
 
@@ -144,26 +288,34 @@ class Monosynth extends React.Component {
     const defaultSlider = {
       'size': [20,120],
       'mode': 'relative',  // 'relative' or 'absolute'
-      'min': 0.000000000000001,
+      'min': 0.01,
       'max': 1,
       'step': 0,
       'value': 0
+    }
+    const pitchSlider = {
+      'size': [20,120],
+      'mode': 'relative',  // 'relative' or 'absolute'
+      'min': 64,
+      'max': 88,
+      'step': 1,
+      'value': 64
     }
 
     const filterFreqSlider = {
       'size': [20,120],
       'mode': 'relative',  // 'relative' or 'absolute'
-      'min': 5,
-      'max': 500,
+      'min': 1,
+      'max': 150,
       'step': 0,
-      'value': 500
+      'value': 150
     }
 
 
 
     // this.dial1 = new Nexus.Dial('#dial')
     // this.dial1.on('change', (val)=>this.handleRadioButtonChange(val, 'envelope.attack') )
-    console.log('this.props.id',this.props.id)
+    // console.log('this.props.id',this.props.id)
 
     this.oscillatorType = new Nexus.RadioButton(`#oscillatorType${this.props.id}`,{
       'size': [120,25],
@@ -173,45 +325,88 @@ class Monosynth extends React.Component {
 
     this.oscillatorType.on('change', (val)=>this.handleRadioButtonChange(val, 'oscillator.type') )
 
-    this.envelopeAttack = new Nexus.Slider(`#envelopeAttack${this.props.id}`,defaultSlider)
-    this.envelopeAttack.value = this.state.envelope.attack
-    this.envelopeAttack.on('change', (val)=>this.handleControlChange(val, 'envelope.attack') )
+    const mainSettings = Object.keys(this.state.settings)
+
+    for (const mainSetting in mainSettings){
+      const main = mainSettings[mainSetting]
+      if(main === 'oscillator') continue
+      if(main === 'filter') continue
+
+      const secondarySettings = Object.keys(this.state.settings[main])
+      for(const secSetting in secondarySettings){
+        const second = secondarySettings[secSetting]
+        if(second === 'octaves') continue
+        if(second === 'exponent') continue
+        console.log('setting',main, second)
+        const name = main+'.'+second
+        if(second === 'baseFrequency'){
+          this[name] = new Nexus.Slider(`#${name + this.props.id}`,filterFreqSlider)
+        }else{
+          this[name] = new Nexus.Slider(`#${name + this.props.id}`,defaultSlider)
+        }
+        this[name].value = this.state.settings[main][second]
+        this[name].on('change', (val)=>this.handleControlChange(val, name) )
+
+      }
+    }
+    /*
+    const eA = 'envelope.attack'
+    const arr = eA.split('.')
+    const partA = arr[0]
+    const partB = arr[1].substr(0,1).toUpperCase()
+    const partC = arr[1].substr(1)
+    const eACamel = [partA, partB, partC].join('')
+    console.log(partA, partB, partC, eACamel)
+
+
+    this[eA] = new Nexus.Slider(`#${eA + this.props.id}`,defaultSlider)
+    this[eA].value = this.state.settings.envelope.attack
+    this[eA].on('change', (val)=>this.handleControlChange(val, eA) )
+
+    // this.envelopeAttack = new Nexus.Slider(`#envelopeAttack${this.props.id}`,defaultSlider)
+    // this.envelopeAttack.value = this.state.settings.envelope.attack
+    // this.envelopeAttack.on('change', (val)=>this.handleControlChange(val, 'envelope.attack') )
 
     this.envelopeDecay = new Nexus.Slider(`#envelopeDecay${this.props.id}`,defaultSlider)
-    this.envelopeDecay.value = this.state.envelope.decay
+    this.envelopeDecay.value = this.state.settings.envelope.decay
     this.envelopeDecay.on('change', (val)=>this.handleControlChange(val, 'envelope.decay') )
 
     this.envelopeSustain = new Nexus.Slider(`#envelopeSustain${this.props.id}`,defaultSlider)
-    this.envelopeSustain.value = this.state.envelope.sustain
+    this.envelopeSustain.value = this.state.settings.envelope.sustain
     this.envelopeSustain.on('change', (val)=>this.handleControlChange(val, 'envelope.sustain') )
 
     this.envelopeRelease = new Nexus.Slider(`#envelopeRelease${this.props.id}`,defaultSlider)
-    this.envelopeRelease.value = this.state.envelope.release
+    this.envelopeRelease.value = this.state.settings.envelope.release
     this.envelopeRelease.on('change', (val)=>this.handleControlChange(val, 'envelope.release') )
 
 
 
     this.filterEnvelopeAttack = new Nexus.Slider(`#filterEnvelopeAttack${this.props.id}`,defaultSlider)
-    this.filterEnvelopeAttack.value = this.state.filterEnvelope.attack
+    this.filterEnvelopeAttack.value = this.state.settings.filterEnvelope.attack
     this.filterEnvelopeAttack.on('change', (val)=>this.handleControlChange(val, 'filterEnvelope.attack') )
 
     this.filterEnvelopeDecay = new Nexus.Slider(`#filterEnvelopeDecay${this.props.id}`,defaultSlider)
-    this.filterEnvelopeDecay.value = this.state.filterEnvelope.decay
+    this.filterEnvelopeDecay.value = this.state.settings.filterEnvelope.decay
     this.filterEnvelopeDecay.on('change', (val)=>this.handleControlChange(val, 'filterEnvelope.decay') )
 
     this.filterEnvelopeSustain = new Nexus.Slider(`#filterEnvelopeSustain${this.props.id}`,defaultSlider)
-    this.filterEnvelopeSustain.value = this.state.filterEnvelope.sustain
+    this.filterEnvelopeSustain.value = this.state.settings.filterEnvelope.sustain
     this.filterEnvelopeSustain.on('change', (val)=>this.handleControlChange(val, 'filterEnvelope.sustain') )
 
     this.filterEnvelopeRelease = new Nexus.Slider(`#filterEnvelopeRelease${this.props.id}`,defaultSlider)
-    this.filterEnvelopeRelease.value = this.state.filterEnvelope.release
+    this.filterEnvelopeRelease.value = this.state.settings.filterEnvelope.release
     this.filterEnvelopeRelease.on('change', (val)=>this.handleControlChange(val, 'filterEnvelope.release') )
 
     this.filterFrequency = new Nexus.Slider(`#filterfrequency${this.props.id}`,filterFreqSlider)
-    this.filterFrequency.value = this.state.filterEnvelope.baseFrequency
+    this.filterFrequency.value = this.state.settings.filterEnvelope.baseFrequency
     this.filterFrequency.on('change', (val)=>this.handleControlChange(val, 'filterEnvelope.baseFrequency') )
-
-
+    */
+    this.beatSliders = []
+    this.state.sequence.forEach((beat,i)=>{
+      this.beatSliders[i] = new Nexus.Slider(`#beat-${this.props.id}-${i}`,pitchSlider)
+      this.beatSliders[i].value = this.state.sequence[i].pitch
+      this.beatSliders[i].on('change', (val)=>this.handlePitchChange(val, i) )
+    })
     // const el = document.querySelector('#target')
     // console.log(el)
     // console.log(this.sequencerElement)
@@ -232,18 +427,26 @@ class Monosynth extends React.Component {
 
         <div id={`oscillatorType${this.props.id}`}></div>
 
-        <div className="control-container">
-          <div id={`envelopeAttack${this.props.id}`}></div>
-          <div id={`envelopeDecay${this.props.id}`}></div>
-          <div id={`envelopeSustain${this.props.id}`}></div>
-          <div id={`envelopeRelease${this.props.id}`}></div>
+        <div className="controls">
+          <div className="control-container">
+            <div id={`envelope.attack${this.props.id}`}></div>
+            <div id={`envelope.decay${this.props.id}`}></div>
+            <div id={`envelope.sustain${this.props.id}`}></div>
+            <div id={`envelope.release${this.props.id}`}></div>
+          </div>
+          <div className="control-container">
+            <div id={`filterEnvelope.attack${this.props.id}`}></div>
+            <div id={`filterEnvelope.decay${this.props.id}`}></div>
+            <div id={`filterEnvelope.sustain${this.props.id}`}></div>
+            <div id={`filterEnvelope.release${this.props.id}`}></div>
+            <div id={`filterEnvelope.baseFrequency${this.props.id}`}></div>
+          </div>
         </div>
-        <div className="control-container">
-          <div id={`filterEnvelopeAttack${this.props.id}`}></div>
-          <div id={`filterEnvelopeDecay${this.props.id}`}></div>
-          <div id={`filterEnvelopeSustain${this.props.id}`}></div>
-          <div id={`filterEnvelopeRelease${this.props.id}`}></div>
-          <div id={`filterfrequency${this.props.id}`}></div>
+
+        <div className="notes">
+          <div className="control-container">
+            {this.state.sequence.map((beat, i)=><div key={i} id={`beat-${this.props.id}-${i}`}></div>)}
+          </div>
         </div>
       </div>
     )
